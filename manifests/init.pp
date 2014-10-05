77daefe7333d2b202cbbc89f97af5b61375af233
James Whiteley <james.whiteley@gmail.com>
2014-10-04 23:21:13 -0500

    Added group 'arcsight' and user 'arcsight' declarations.

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,5 +11,21 @@
 # Sample Usage:
 #
 class arcsight {
-
+  group { "arcsight" :
+    ensure => present,
+    gid => 8843,
+  }
+    
+  user { "arcsight" :
+    ensure => present,
+    uid => 8843,
+    managehome => true,
+    shell => '/bin/bash',
+    expiry => absent,
+    gid => 'arcsight',
+    password => '!!'
+  }
+  
+  # Only for arcsight::esm file { '/opt/arcsight'
+  # arcsight::connector should be "file '$ERCOT_APPS/ArcSightSmartConnectors'
 }
